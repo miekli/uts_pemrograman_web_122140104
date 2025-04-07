9849cbc02ddb50858462a2f4a2642fcd0fd1107a
miekli <128711146+miekli@users.noreply.github.com>
2025-04-07 20:10:21 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,8 +9,9 @@ function Profile() {
       <div className="profile-card">
         <img src="/images/profile.jpg" alt="User Profile" className="profile-image" />
         <div className="profile-info">
-          <p><strong>Nama:</strong> John Doe</p>
-          <p><strong>Email:</strong> john@example.com</p>
+          <p><strong>Nama:</strong> Mychael Daniel N</p>
+          <p><strong>Nim:</strong> 122140104 </p>
+          <p><strong>Email:</strong> mychael.122140104@student.itera.ac.id</p>
           <p><strong>Total Tiket Dibeli:</strong> 3</p>
           <div className="profile-extra">
             <span className="coins"><FaCoins /> 120 Coin</span>
